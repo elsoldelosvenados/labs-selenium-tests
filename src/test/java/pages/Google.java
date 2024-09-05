7ead7028970ed27af85dff911fc39dda0e92ff4e
Andres Acosta <andresakosta@outlook.com>
2024-09-04 21:05:02 -0500

    Adds timeouts

--- src/test/java/pages/Google.java
+++ src/test/java/pages/Google.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.time.Duration;
 import java.util.logging.Logger;
 
 public class Google extends PageObjectBase {
@@ -10,6 +11,8 @@ public class Google extends PageObjectBase {
     private final Logger logger =  Logger.getLogger(Google.class.getName());
     public void navigateToHome(){
         try{
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
            driver.get("https://google.com");
            Thread.sleep(3000);
            logger.info("INSIDE THE CHROME INSTANCE.");
